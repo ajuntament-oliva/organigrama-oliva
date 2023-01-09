e8c0bc6cd30efd0130a54ec3397c90e76e1c7dfb
ajuntament-oliva <117634881+ajuntament-oliva@users.noreply.github.com>
2023-01-09 13:04:44 +0100

    ghj
    dryj

---
+++ index.js
@@ -0,0 +1,27 @@
+const mostrarR=document.getElementById("bMostrarResponsive");
+mostrarR.addEventListener("click",()=>{mostrarLlista()});
+const showResponsive=document.getElementById("showResponsive");
+const show=document.getElementById("show");
+
+function mostrarLlista(){
+    showResponsive.classList.toggle("d-none"); 
+    if(mostrarR.textContent==="Mostrar index"){
+        mostrarR.textContent="No mostrar index";
+    }else{
+        mostrarR.textContent="Mostrar index.";
+
+    }
+}
+
+const mostrar=document.getElementById("bMostrar");
+mostrar.addEventListener("click",()=>{mostrarLlistaEscritori()});
+console.log();
+function mostrarLlistaEscritori(){
+    show.classList.toggle("d-none"); 
+    if(mostrar.textContent==="Mostrar index"){
+        mostrar.textContent="No mostrar index";
+    }else{
+        mostrar.textContent="Mostrar index.";
+
+    }
+}
